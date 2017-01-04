5b95d826d800bd0e89c9f63b2d6d5e5588fad262
Kang <2881570+kangni@users.noreply.github.com>
2017-01-04 14:22:25 +0800

    Create table User

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask.ext.script import Manager, Server
-from main import app
+from main import app, db, User
+
 
 manager = Manager(app)
 
@@ -9,7 +10,7 @@ manager.add_command("server", Server())
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db, User=User)
 
 if __name__ == '__main__':
     manager.run()
