94dfbab2b1ba7ab2d185f2eba86c99e50a1fd27e
Kang <2881570+kangni@users.noreply.github.com>
2017-01-06 17:20:24 +0800

    add Comment && Tag model

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
 
+tags = db.Table(
+    'post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -31,12 +37,35 @@ class Post(db.Model):
     text = db.Column(db.Text())
     publish_date = db.Column(db.DateTime())
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    tags = db.relationship('Tag', secondary=tags, backref='posts')
 
     def __init__(self, title):
         self.title = title
 
     def __repr__(self):
         return "<Post '{}'>".format(self.title)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
 
 
 @app.route('/')
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask.ext.script import Manager, Server
-from main import app, db, User, Post
+from main import app, db, User, Post, Tag
 
 
 manager = Manager(app)
@@ -10,7 +10,7 @@ manager.add_command("server", Server())
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post)
+    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag)
 
 if __name__ == '__main__':
     manager.run()
