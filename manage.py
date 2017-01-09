9243fe565ffff0fc0a02146e4ca34fd8051513df
Kang <2881570+kangni@users.noreply.github.com>
2017-01-09 13:45:46 +0800

    initial migration

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask.ext.script import Manager, Server
 from main import app, db, User, Post, Tag
+from flask.ext.migrate import Migrate, MigrateCommand
 
+migrate = Migrate(app, db)
 
 manager = Manager(app)
-
 manager.add_command("server", Server())
+manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
