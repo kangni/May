811690cad510bf2ed10aad0edbad4122e5d0544a
Kang <2881570+kangni@users.noreply.github.com>
2017-01-12 14:29:56 +0800

    Register Form

--- May/forms.py
+++ May/forms.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask_wtf import Form
+from flask_wtf import Form, RecaptchaField
 
 from wtforms import StringField, TextAreaField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, URL
@@ -37,6 +37,28 @@ class LoginForm(Form):
 
         if not user.check_password(self.password.data):
             self.username.errors.append('Invalid username or password')
+            return False
+
+        return True
+
+
+class RegisterForm(Form):
+    username = StringField('Username', [DataRequired(), Length(max=255)])
+    password = PasswordField('Password', [DataRequired(), Length(min=8)])
+    confirm = PasswordField('Confirm', [DataRequired(), EqualTo('password')])
+
+    recaptcha = RecaptchaField()
+
+    def validate(self):
+        check_validate = super(RegisterForm, self).validate()
+
+        if not check_validate:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+
+        if user:
+            self.username.errors.append("User with that name already in use.")
             return False
 
         return True
