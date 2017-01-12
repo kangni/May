1d2f8aa9d6479a4dbfdbf3c45a0bdc9411926be1
Kang <2881570+kangni@users.noreply.github.com>
2017-01-12 09:57:03 +0800

    add Post Form

--- May/forms.py
+++ May/forms.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import Form
 
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextAreaField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, URL
 
 
 class CommentForm(Form):
@@ -11,3 +11,8 @@ class CommentForm(Form):
         validators=[DataRequired(), Length(max=225)]
     )
     text = TextAreaField(u'Comment', validators=[DataRequired()])
+
+
+class PostForm(Form):
+    title = StringField('Title', [DataRequired(), Length(max=255)])
+    text = TextAreaField('Content', [DataRequired()])
