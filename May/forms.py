db1c95e7ed3639746531de23563f96cf28323a8e
Kang <2881570+kangni@users.noreply.github.com>
2017-01-12 10:28:04 +0800

    add Login Form

--- May/forms.py
+++ May/forms.py
@@ -3,6 +3,7 @@ from flask_wtf import Form
 
 from wtforms import StringField, TextAreaField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, URL
+from May.models import User
 
 
 class CommentForm(Form):
@@ -16,3 +17,26 @@ class CommentForm(Form):
 class PostForm(Form):
     title = StringField('Title', [DataRequired(), Length(max=255)])
     text = TextAreaField('Content', [DataRequired()])
+
+
+class LoginForm(Form):
+    username = StringField('Username', [DataRequired(), Length(max=255)])
+    password = PasswordField('Password', [DataRequired()])
+    remember = BooleanField("Remember Me")
+
+    def validate(self):
+        check_validate = super(LoginForm, self).validate()
+
+        if not check_validate:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+        if not user:
+            self.username.errors.append('Invalid username or password')
+            return False
+
+        if not user.check_password(self.password.data):
+            self.username.errors.append('Invalid username or password')
+            return False
+
+        return True
